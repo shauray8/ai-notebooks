29b440bb03a0358f481c4f2f885c5a28ae690b4e
Shauray <shauray9@gmail.com>
2023-05-21 00:07:42 +0530

    parsing

--- similarity-search/gather.py
+++ similarity-search/gather.py
@@ -2,6 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 
 
+## convert to class maybe
+
+def parse_data(site, URL):
+
+    if site == "myntraa":
+        return myntraa(URL)
+    if site == "amazon":
+        return amazon(URL)
+    if site == "ajio":
+        return ajio(URL)
+    if site == "nykaa":
+        return nykaa(URL)
+
+    return None
+
+
 def myntraa():
     pass
 
@@ -11,39 +27,25 @@ def amazon():
 def ajio():
     pass
 
-def other():
-    pass
-
-# Send a GET request to the Nykaa search page
-url = "https://www.nykaa.com/search/result/?q=Clothing"
-response = requests.get(url)
+def nykaa(URL):
+    url = "https://www.nykaa.com/search/result/?q=Clothing"
+    response = requests.get(url)
 # Create BeautifulSoup object to parse the HTML content
-soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, 'html.parser')
 # Find clothing items on the page
-clothing_items = soup.find_all('div', {'class': 'css-d5z3ro'})
+    clothing_items = soup.find_all('div', {'class':'css-ifdzs8'})
 # Iterate over each clothing item and extract relevant information
-for item in clothing_items:
-    info = item.find_all('div', {'class': 'item'})
-    print(info)
-    # Extract product name
-    product_name = item.find('a', {'class': 'product-title'}).text.strip()
+    for item in clothing_items:
+        # Extract product name
+        product_url = item.find('a', {'class': 'css-qlopj4'}).get("href")
+        # Print the extracted information
+        product_response = requests.get("https://nykaa.com/"+product_url)
+        product_soup = BeautifulSoup(product_response.text, 'html.parser')
+        disc = product_soup.find('div', class_ = "description-expand css-98gl1e")
+        print("Product URL:", disc)
 
-    # Extract product brand
-    product_brand = item.find('a', {'class': 'product-brand'}).text.strip()
+        print("----------------------------------------")
+        pass
 
-    # Extract product price
-    product_price = item.find('span', {'class': 'product-price'}).text.strip()
 
-    # Extract product image URL
-    product_image_url = item.find('img', {'class': 'product-img'})['src']
-
-    # Extract product URL
-    product_url = item.find('a', {'class': 'product-img-link'})['href']
-
-    # Print the extracted information
-    print("Product Name:", product_name)
-    print("Brand:", product_brand)
-    print("Price:", product_price)
-    print("Image URL:", product_image_url)
-    print("Product URL:", product_url)
-    print("----------------------------------------")
+nykaa("S")
--- similarity-search/main.py
+++ similarity-search/main.py
@@ -1,9 +1,25 @@
 import numpy as np
-import torch 
+import argparse
 from embedd import create_embeddings
+from gather import *
 
+parser = argparse.ArgumentParser(description='PyTorch U square net training on comma 10k dataset',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
+parser.add_argument('--mode', default="test", type=str,
+                    help='operations', choices=["train","test"])
+
+parser.add_argument('--prompt', default=None, type=str,
+                    help='define a prompt, different functionallity for both train and test')
+
+parser.add_argument('--site', default=None, type=str,
+                    help='Site to parse data from, diff functions for every website')
 
 if __name__ == "__main__":
-    print(create_embeddings("hee"))
+    args = parser.parse_args()
+    if args.mode == "test":
+        print(create_embeddings(args.prompt))
+
+    else:
+        parse_data(args.site, args.prompt)
     pass
