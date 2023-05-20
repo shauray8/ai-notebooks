3d34c8c037f3d81af744f92126a18e4f9ef66977
Shauray <shauray9@gmail.com>
2023-05-20 23:23:14 +0530

    compiling

--- similarity-search/embedd.py
+++ similarity-search/embedd.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer
-sentences = ["Twenty Dresses by Nykaa Fashion Navy Blue Slogan"]
 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-embeddings = model.encode(sentences)
-print(len(embeddings[0]))
+
+def create_embeddings(prompt):
+    embeddings = model.encode([prompt])
+    return embeddings[0]
---
+++ similarity-search/main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import torch 
+from embedd import create_embeddings
+
+
+
+if __name__ == "__main__":
+    print(create_embeddings("hee"))
+    pass
