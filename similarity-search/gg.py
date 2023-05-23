0bb2bd5eed3023adedd7668b6c40a0b93ccd9d82
Shauray <shauray9@gmail.com>
2023-05-23 10:28:32 +0530

    does not work still

--- similarity-search/gg.py
+++ similarity-search/gg.py
@@ -1,1 +1,35 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+# Set the path to your chromedriver executable
+webdriver_service = Service('path/to/chromedriver')
+
+# Set the options for the Chrome browser
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')  # Run Chrome in headless mode
+
+# Create a new instance of the Chrome driver
+driver = webdriver.Chrome(service=webdriver_service, options=options)
+
+# Set the URL of the page to scrape
+page_url = 'https://www.myntra.com/clothing?src=bc&p=1'
+
+# Navigate to the page
+driver.get(page_url)
+
+# Find all the links on the page
+links = driver.find_elements(By.TAG_NAME, 'a')
+
+# Extract the href attribute from each link
+page_links = [link.get_attribute('href') for link in links]
+
+# Filter the links to include only those starting with 'https://www.myntra.com/'
+myntra_links = [link for link in page_links if link.startswith('https://www.myntra.com/')]
+
+# Print the links
+for link in myntra_links:
+    print(link)
+
+# Quit the driver
+driver.quit()
--- similarity-search/ss.py
+++ similarity-search/ss.py
@@ -1,34 +1,37 @@
-from selenium import webdriver
+import requests, json
 from bs4 import BeautifulSoup
 
-# Set the path to the ChromeDriver executable
-chrome_driver_path = "/path/to/chromedriver"  # Replace with the actual path to chromedriver
+headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'}
 
-# Set up the Selenium ChromeDriver
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")  # Run Chrome in headless mode
-driver = webdriver.Chrome(chrome_driver_path, options=options)
+def my():
+    s = requests.Session()
+    res = s.get("https://www.myntra.com/sports-shoes/puma/puma-men-blue-hybrid-fuego-running-shoes/11203218/buy", headers=headers, verify=False)
 
-# Load the webpage
-url = "https://www.myntra.com/clothing?src=bc&p=1"
-driver.get(url)
+    soup = BeautifulSoup(res.text,"lxml")
 
-# Wait for the content to load (adjust the wait time as needed)
-driver.implicitly_wait(5)
+    script = None
+    for s in soup.find_all("script"):
+        if 'pdpData' in s.text:
+            script = s.get_text(strip=True)
+            break
 
-# Get the page source after the content is loaded
-html_content = driver.page_source
+    a = json.loads(script[script.index('{'):])
+    print(a["pdpData"]["productDetails"])
 
-# Parse the HTML content with BeautifulSoup
-soup = BeautifulSoup(html_content, 'html.parser')
-print(soup)
 
-# Find and extract the desired information
-# Example: Get all the paragraph tags
-paragraph_tags = soup.find_all('p')
+def some():
+    s = requests.Session()
+    res = s.get("https://www.nykaa.com/twenty-dresses-by-nykaa-fashion-basics-navy-blue-slogan-printed-crop-tshirt/p/6583572?productId=6583572&pps=2&skuId=6583557", verify=False)
 
-for p in paragraph_tags:
-    print(p.get_text())
+    soup = BeautifulSoup(res.text,"lxml")
 
-# Quit the driver
-driver.quit()
+    script = None
+    for s in soup.find_all("script"):
+        if 'pdpData' in s.text:
+            script = s.get_text(strip=True)
+            break
+
+    a = json.loads(script[script.index('{'):])
+    print(a)
+
+some()
